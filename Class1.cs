bb3c7d73f464dd2cd6a717ed14e5f087d0f0d6bb
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-08-27 16:18:49 -0400

    S2V2ii

---
+++ Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        var mysteryMessage = new byte[] { 89, 0, 97, 0, 121, 0, 33, 0 };
+        var messageContents = UnicodeEncoding.Unicode.GetString(mysteryMessage);
+    }
+}
